00812902bcfa7353c8fa1f67968daa77182871ae
Derek Roode <droode@nexient.com>
2021-12-17 18:07:41 -0500

    fixed mobile styling as well as desktop issue for login page - added device scale to html

--- react-app/src/components/atoms/ButtonLarge/ButtonLarge.js
+++ react-app/src/components/atoms/ButtonLarge/ButtonLarge.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './ButtonLarge.module.css'
 
 function ButtonLarge({ text, buttonLargeOnClick }) {
   return (
-    <div>
+    <div className={styles.ButtonLarge_container}>
       <button onClick={buttonLargeOnClick}>{text}</button>
     </div>
   );
--- react-app/src/components/organisms/LoginComponent/LoginComponent.js
+++ react-app/src/components/organisms/LoginComponent/LoginComponent.js
@@ -8,11 +8,14 @@ import styles from './LoginComponent.module.css'
 function LoginComponent() {
   return (
   <div className={styles.LoginComponent_container}>
+    <div className={styles.LoginComponent_container__contents}>
+
       <Logo/>
       <LoginForm/>
       <h2>Don't have an account? 
       <a href='/signup'>Sign Up</a>  
       </h2>
+    </div>
       
       
     </div>
